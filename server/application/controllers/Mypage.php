4db1cb9b229d4edbe3079940823e8711b33e2647
masaka <masaka@mz-kb.com>
2018-10-27 16:10:22 +0900

    Redirect to login from mypage

--- server/application/controllers/Mypage.php
+++ server/application/controllers/Mypage.php
@@ -9,8 +9,12 @@ class Mypage extends CI_Controller {
 
 	function index()
 	{
-		$this->smarty->view('mypage.tpl', array(
-			'user' => isset($_SESSION['user_id']) ? $this->user->get('*', array('id' => $_SESSION['user_id'])) : null,
-		));
+		if (!isset($_SESSION['user_id']) || ($this->user->count(array('id' => $_SESSION['user_id'])) === 0)) {
+			redirect('/login');
+		} else {
+			$this->smarty->view('map.tpl', array(
+				'user' => $this->user->get('*', array('id' => $_SESSION['user_id'])),
+			));
+		}
 	}
 }
