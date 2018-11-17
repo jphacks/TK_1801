69bc740af5b682494287d70ae14779b6b5957b40
masaka <masaka@mz-kb.com>
2018-11-17 23:00:41 +0900

    Fix profile image path

--- server/application/controllers/Profile_image.php
+++ server/application/controllers/Profile_image.php
@@ -10,9 +10,9 @@ class Profile_image extends CI_Controller {
 	{
 		header('Content-Type: image/jpeg');
 
-		$user = $this->user->get('profile_image_filename', array('id' => $_SESSION['user_id']));
+		$user = $this->user->get('profile_image_filename', array('id' => $user_id));
 		if ($user === false || !isset($user['profile_image_filename'])) {
-			readfile('images/no_image.jpg');
+			readfile(APPPATH.'../images/no_image.jpg');
 		} else {
 			// TODO: 変な値を入れられた時のセキュリティ対策
 			readfile($user['profile_image_filename']);
