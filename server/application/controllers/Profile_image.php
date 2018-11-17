730630338b7dc280ba82845a500153b9ba68eee1
masaka <masaka@mz-kb.com>
2018-11-17 23:01:48 +0900

    Debug

--- server/application/controllers/Profile_image.php
+++ server/application/controllers/Profile_image.php
@@ -8,14 +8,14 @@ class Profile_image extends CI_Controller {
 
 	function index($user_id = 0)
 	{
-		header('Content-Type: image/jpeg');
-
-		$user = $this->user->get('profile_image_filename', array('id' => $user_id));
+		//header('Content-Type: image/jpeg');
+		var_dump($user_id);
+		/*$user = $this->user->get('profile_image_filename', array('id' => $user_id));
 		if ($user === false || !isset($user['profile_image_filename'])) {
 			readfile(APPPATH.'../images/no_image.jpg');
 		} else {
 			// TODO: 変な値を入れられた時のセキュリティ対策
 			readfile($user['profile_image_filename']);
-		}
+		}*/
 	}
 }
