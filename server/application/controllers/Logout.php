53ec15d166d9aef4c43d2c315fd278d4ad60c317
masaka <masaka@mz-kb.com>
2018-11-18 02:54:56 +0900

    Add logout controller

---
+++ server/application/controllers/Logout.php
@@ -0,0 +1,22 @@
+<?php
+class Login extends CI_Controller {
+
+	function __construct()
+	{
+		parent::__construct();
+		session_start();
+	}
+
+	function index()
+	{
+		// セッションを解放
+		session_unset();
+		session_destroy();
+	  
+		// 念のため新しいセッションIDを振り直す
+		session_regenerate_id(true);
+
+		// トップへ
+		redirect('/');
+	}
+}
