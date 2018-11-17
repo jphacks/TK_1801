646891932c0e11cc13d1fae4d81b6e62e8796377
masaka <masaka@mz-kb.com>
2018-11-18 02:02:12 +0900

    Fix column name

--- server/application/controllers/Notifications.php
+++ server/application/controllers/Notifications.php
@@ -17,7 +17,7 @@ class Notifications extends CI_Controller {
 			redirect('/login');
 		} else {
 			$reviews = $this->guidance_review->search(
-				'guidances.tourist_user_id AS tourist_user_id, '.
+				'guidances.customer_user_id AS tourist_user_id, '.
 				'users.name AS tourist_user_name, '.
 				'guidance_reviews.body AS body, '.
 				'guidance_reviews.created_at AS created_at',
